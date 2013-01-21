d7d83323234cccfc38fccd1b1f3d2c6da4af2ce8
hregnie2 <hregnie2@adm-info03>
2013-01-21 16:58:34 +0100

    Starting routing

--- src/Sidus/SidusBundle/Controller/DefaultController.php
+++ src/Sidus/SidusBundle/Controller/DefaultController.php
@@ -4,10 +4,20 @@ namespace Sidus\SidusBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('SidusBundle:Default:index.html.twig', array('name' => $name));
-    }
+class DefaultController extends Controller {
+
+	public function viewAction($node) {
+		$id_node = null;
+		$slug = null;
+		if (is_numeric($node)) {
+			$id_node = $node;
+		} else if (is_string($node)) {
+			$slug = $node;
+		} else {
+			return $this->render('SidusBundle:Default:error.html.twig', array('id_node' => $id_node));
+		}
+
+		return $this->render('SidusBundle:Default:view.html.twig', array('id_node' => $id_node, 'slug' => $slug));
+	}
+
 }
